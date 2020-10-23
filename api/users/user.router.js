f45c815d1be71ba11724eb97b53cf4445e4c67ab
DIU <shawon3719@gmail.com>
2020-10-23 22:16:39 +0600

    added Auth middleware to verify access token

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -7,12 +7,13 @@ const {
   login,
 } = require("./user.controller");
 const router = require("express").Router();
+const { checkToken } = require("../../auth/token_validation");
 
-router.post("/create", createUser);
-router.get("/all", getUsers);
-router.get("/user/:id", getUserByUserId);
-router.patch("/update", updateUser);
-router.delete("/delete", deleteUser);
+router.post("/create", checkToken, createUser);
+router.get("/all", checkToken, getUsers);
+router.get("/user/:id", checkToken, getUserByUserId);
+router.patch("/update", checkToken, updateUser);
+router.delete("/delete", checkToken, deleteUser);
 router.post("/login", login);
 
 module.exports = router;
